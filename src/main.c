0efea64bf124975f72d907db6f30121b09916892
xoreo <mattnappo@gmail.com>
2018-12-02 22:19:05 -0500

    added openssl headers

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <openssl/applink.c>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+/*
+    https://stackoverflow.com/questions/7698488/turn-a-simple-socket-into-an-ssl-socket
+*/
+
 int main(int argc, char **args) {
     printf("main\n");
-    
+
     return 0;
 }
