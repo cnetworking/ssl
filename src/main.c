5f57664abab316a70d7c5654af5cb04ee8209688
xoreo <mattnappo@gmail.com>
2018-12-03 07:45:20 -0500

    Added basic ssl functions

--- src/main.c
+++ src/main.c
@@ -10,6 +10,22 @@
     https://stackoverflow.com/questions/7698488/turn-a-simple-socket-into-an-ssl-socket
 */
 
+void initialize_ssl() {
+    SSL_load_error_strings();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+}
+
+void destroy_ssl() {
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+void shutdown_ssl() {
+    SSL_shutdown(c_ssl);
+    SSL_free(c_ssl);
+}
+
 int main(int argc, char **args) {
     printf("main\n");
 
